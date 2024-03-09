a29f518e735466bd7084fdd13628ad7ed85af25d
Jhowzxcv <jhodourado131@gmail.com>
2024-03-09 18:05:27 -0300

    padronização e ajustes

--- celulares/script.js
+++ celulares/script.js
@@ -19,16 +19,36 @@ document.getElementById("Btn2").addEventListener("click", redirecionar2);
 document.getElementById("Btn3").addEventListener("click", redirecionar3);
 document.getElementById("Btn4").addEventListener("click", redirecionar4);
 
-function search_animal() {
-    let input = document.getElementById('searchbar').value;
-    input = input.toLowerCase();
+function search_product() {
+    let input = document.getElementById('searchbar').value.toLowerCase();
     let produtos = document.getElementsByClassName('product');
-      
+    let produtoEncontrado = false; // Variável para verificar se pelo menos um produto foi encontrado
+    
+    // Exibir todos os produtos antes de começar a nova pesquisa
+    for (let i = 0; i < produtos.length; i++) { 
+        produtos[i].style.display = "block";
+    }
+    
     for (let i = 0; i < produtos.length; i++) { 
         if (!produtos[i].textContent.toLowerCase().includes(input)) {
             produtos[i].style.display = "none";
         } else {
-            produtos[i].style.display = "block";  // Alterado para "block" para mostrar o elemento
+            produtoEncontrado = true; // Um produto foi encontrado
+        }
+    }
+    
+    // Se nenhum produto foi encontrado, atualiza o conteúdo da div 'resultado'
+    if (!produtoEncontrado) {
+        let divResultado = document.getElementById('resultado');
+        if (divResultado) {
+            divResultado.textContent = 'Produto não encontrado';
+        }
+    } else {
+        // Se algum produto foi encontrado, remove o conteúdo da div 'resultado' se existir
+        let divResultado = document.getElementById('resultado');
+        if (divResultado) {
+            divResultado.textContent = ''; // Remove o conteúdo da div
         }
     }
 }
+
